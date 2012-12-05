cbba3b1606db6f486beb6d3fc33aa5b1fe433adc
Ken Mickles <ken@37i.net>
2012-12-04 20:51:25 -0500

    week5 comments

--- queries.sql
+++ queries.sql
@@ -1,4 +1,5 @@
 
+# add a product to the database
 insert into products (name, description, image, price) values (
 	'Angry Leopard Seal', 
 	'Lorem ipsum adipisicing sint consequat do veniam ea. Lorem ipsum dolore sit eu dolore ut est eiusmod.', 
@@ -6,20 +7,28 @@ insert into products (name, description, image, price) values (
 	12
 )
 
+# select all products, sorting alphabetically by name 
 select * from products order by name
 
+# select the first 2 products
 select * from products limit 2
 
+# select the second 2 products
 select * from products limit 2, 2
 
+# select all columns from the product with the id # 6
 select * from products where id = 6
 
+# select the id and name columns from the product with the id # 6
 select id, name from products where id = 6
 
+# select the id and name columns from all products with a price of 12
 select id, name from products where price = 12
 
+# select products with a price of 12 and the (case insensitive) name "Angry Leopard Seal"
 select * from products where price = 12 and name = "Angry Leopard Seal"
 
+# select products with a price of 12 OR the (case insensitive) name "Angry Leopard Seal"
 select * from products where price = 12 or name = "Angry Leopard Seal"
 
 # match products with the price 12 whose names end with t-shirt
@@ -28,8 +37,10 @@ select * from products where price = 12 and name like "%t-shirt"
 # match products with t-shirt in the name
 select * from products where name like "%t-shirt%"
 
+# select products that were created within the last hour
 select * from products where created_at > (NOW( ) - INTERVAL 1 HOUR)
 
+# select products created between two dates
 select * from products where created_at BETWEEN "2012-12-01" AND "2012-12-31"
 
 # update the name of product #6
--- test.php
+++ test.php
@@ -1,20 +1,26 @@
 <?php
 
-$user = 'bucksphp';
+// our mysql connection info
+$user = 'bucksphps';
 $password = 'fQ4fRD5CVFXmCw9F';
 $database = 'bucksphp_store';
-$host = '127.0.0.1';
+$host = 'localhost';
 
+// connect to the mysql server
 $db = new mysqli($host, $user, $password, $database);
 
+// if the connection fails, an error message will be set
+// check for that error message and kill the script if it exists
 if ( $db->connect_errno ) {
-	die("Failed to connect to database: " . $db->connect_errno);
+	die("Failed to connect to database: " . $db->connect_error);
 }
 
-
+// fetch all products, ordering alphabetically by name
 $sql = "SELECT * FROM products ORDER BY name";
 $result = $db->query($sql);
 
+// loop through each product row, fetch an associative array, 
+// and print the result in a <pre> tag
 while ( $row = $result->fetch_assoc() ) {
 	echo '<pre>';
 	print_r($row);
