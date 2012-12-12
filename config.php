fa787b73362e5d240cf3d42e2df7cc57aa2eeb8c
Ken Mickles <ken@37i.net>
2012-12-11 21:21:50 -0500

    week6 end of class

--- config.php
+++ config.php
@@ -1,7 +1,18 @@
 <?php
 
 // our mysql connection info
-define('DB_USER', 'bucksphp');
-define('DB_PASSWORD', 'fQ4fRD5CVFXmCw9F');
-define('DB_NAME', 'bucksphp_store');
-define('DB_HOST', 'localhost');
+
+// dev environment
+if ( $_SERVER['SERVER_NAME'] == 'localhost' ) {
+	define('DB_USER', 'bucksphp');
+	define('DB_PASSWORD', 'fQ4fRD5CVFXmCw9F');
+	define('DB_NAME', 'bucksphp_store');
+	define('DB_HOST', 'localhost');
+}
+// production environment
+else {
+	define('DB_USER', 'bucksphp');
+	define('DB_PASSWORD', '3nKB4VQ2FQjJ86c86w');
+	define('DB_NAME', 'bucksphp_store');
+	define('DB_HOST', 'mysql.bucksphp.com');
+}
