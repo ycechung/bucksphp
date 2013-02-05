34a33d815d1e0a8448ffb0fb8921aceaa77d81b8
Ken Mickles <ken@37i.net>
2013-02-04 23:15:35 -0600

    week 11 comments

--- includes/DB.php
+++ includes/DB.php
@@ -1,10 +1,32 @@
 <?php
 
+/**
+ * Database wrapper class
+ *
+ **/
 class DB {
+	/**
+	 * Internal MySQLi connection
+	 *
+	 * @var mysqli
+	 **/
 	private $conn;
 
+	/**
+	 * The ID of the last created record (used to maintain compatability with mysqli)
+	 *
+	 * @var integer
+	 **/
 	public $insert_id = null;
 
+	/**
+	 * Create database connection. Throws generic exception on connect error.
+	 *
+	 * @var string $host - Database host
+	 * @var string $user - Database username
+	 * @var string $password - Database password
+	 * @var string $database - Database name
+	 **/
 	public function __construct($host, $user, $password, $database) {
 		// connect to the mysql server
 		$this->conn = new mysqli($host, $user, $password, $database);
@@ -16,6 +38,12 @@ class DB {
 		}
 	}
 
+	/**
+	 * Escape a string for use in a query
+	 *
+	 * @var string $str - String to escape
+	 * @return string
+	 **/
 	public function escape($str) {
 		// if the string is null, use the MySQL NULL constant
 		if ( is_null($str) ) {
@@ -27,6 +55,12 @@ class DB {
 		}
 	}
 
+	/**
+	 * Select a single record based on a given query
+	 *
+	 * @var string $sql - The SQL query
+	 * @return array
+	 **/
 	public function selectOne($sql) {
 		if ( $result = $this->conn->query($sql) ) {
 			if ( $result->num_rows > 0 ) {
@@ -37,6 +71,12 @@ class DB {
 		return false;
 	}
 
+	/**
+	 * Select multiple records based on a given query
+	 *
+	 * @var string $sql - The SQL query
+	 * @return array
+	 **/
 	public function selectAll($sql) {
 		$result = $this->conn->query($sql);
 
@@ -52,12 +92,25 @@ class DB {
 		return $rows;
 	}
 
+	/**
+	 * Perform a database query and return the result
+	 *
+	 * @var string $sql - The SQL query
+	 * @return mixed
+	 **/
 	public function query($sql) {
 		$result = $this->conn->query($sql);
 		$this->insert_id = $this->conn->insert_id;
 		return $result;
 	}
 
+	/**
+	 * Forward all methods that don't exist on to the internal mysqli connection
+	 *
+	 * @var string $method - Method name
+	 * @var array $args - (optional) Arguments to pass to method
+	 * @return mixed
+	 **/
 	public function __call($method, $args=array()) {
 		return call_user_func_array(array($this->conn, $method), $args);
 	}
